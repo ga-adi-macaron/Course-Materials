9b5872a117702853c9c879b35bf47bc46e2af0a2
drew-mahrt <dmmahrt@comcast.net>
2016-12-13 09:18:51 -0500

    adding sorting algorithms lesson

---
+++ lessons/computer-science-and-interview-prep/sorting-algorithms-lesson/starter-code/BubbleSort.java
@@ -0,0 +1,20 @@
+package BubbleSort;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] arr = {10, 15, 1, 3, 14, 2, 17, 9, 51, 6, 16, 22, 8};
+		bubbleSort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+	private static void bubbleSort(int[] input) {
+		//Your code here. Include a print statement
+		//declaring after which pass (iteration through
+		//the outside loop) the algorithm. It should be the 9th.		
+	}
+
+}
---
+++ lessons/computer-science-and-interview-prep/sorting-algorithms-lesson/starter-code/InsertionSort.java
@@ -0,0 +1,19 @@
+package InsertionSort;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int[] arr = {7, 3, 2, 4, 9, 1, 14, 12};
+		doInsertionSort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+	private static void doInsertionSort(int[] input) {
+		//Your code here
+		
+	}
+}
